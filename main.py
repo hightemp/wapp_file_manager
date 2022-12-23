e583723ab21822abbae79504844742d42e67f904
Панов Антон <hightemp.unknown@gmail.com>
2022-12-23 05:04:00 +0300

    Fri Dec 23 05:04:00 AM MSK 2022 update

--- main.py
+++ main.py
@@ -24,8 +24,80 @@ def get_db():
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
+        # with app.open_resource('schema.sql', mode='r') as f:
+        #     db.cursor().executescript(f.read())
+        sSQL = """
+DROP TABLE IF EXISTS tabs;
+CREATE TABLE tabs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    path TEXT NOT NULL,
+    selected_file TEXT NULL
+);
+
+DROP TABLE IF EXISTS tabs_history;
+CREATE TABLE tabs_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tab_id INTEGER NULL,
+    path TEXT NOT NULL,
+    selected_file TEXT NULL
+);
+
+INSERT INTO tabs (title, path) VALUES ("test 1", "/home/hightemp");
+INSERT INTO tabs (title, path) VALUES ("test 2", "/");
+INSERT INTO tabs (title, path) VALUES ("test 3", "/home/hightemp/Pictures/");
+INSERT INTO tabs (title, path) VALUES ("test 4", "/home/hightemp/.config/");
+
+DROP TABLE IF EXISTS rsync_sync;
+CREATE TABLE rsync_sync (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NULL
+);
+
+DROP TABLE IF EXISTS rsync_options;
+CREATE TABLE rsync_options (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    rsync_sync_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    value TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS rsync_sync_processes;
+CREATE TABLE rsync_sync_processes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    rsync_sync_id INTEGER NOT NULL,
+    exit_code INTEGER NULL,
+    created_at INTEGER NOT NULL,
+    stopped_at INTEGER NULL,
+    progress INTEGER NOT NULL,
+    stdout TEXT NULL,
+    stderror TEXT NULL
+);
+
+DROP TABLE IF EXISTS fav_groups;
+CREATE TABLE fav_groups (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS fav_categories;
+CREATE TABLE fav_categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    group_id INTEGER NULL,
+    parent_id INTEGER NULL
+);
+
+DROP TABLE IF EXISTS fav_files;
+CREATE TABLE fav_files (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    path TEXT NOT nULL,
+    category_id INTEGER NULL
+);
+        """
+
+        db.cursor().executescript(sSQL)
         db.commit()
 
 @app.teardown_appcontext
